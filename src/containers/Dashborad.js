8ea9f208a1192ae1027011df61fc77f16229aec8
LL\rahshind <rahul.shinde@digitaslbi.com>
2017-08-16 12:42:38 +0530

    Added Static Content in Dashboard component

--- src/containers/Dashborad.js
+++ src/containers/Dashborad.js
@@ -2,9 +2,96 @@ import React from 'react';
 
 const Dashboard = () => (
   <div className="container">
-
-    <h2>This is Dashboard</h2>
-
+    <div className="row">
+      <div className="xs-12">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Item Code</th>
+              <th>Item Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Max. Order</th>
+              <th>Category</th>
+              <th>Date Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>AB123</td>
+              <td>Sony Charger</td>
+              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
+              <td>400</td>
+              <td>20</td>
+              <td>4</td>
+              <td>Mobile Accessories</td>
+              <td>16.45, 17 July</td>
+            </tr>
+            <tr>
+              <td>AB123</td>
+              <td>Sony Charger</td>
+              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
+              <td>400</td>
+              <td>20</td>
+              <td>4</td>
+              <td>Mobile Accessories</td>
+              <td>16.45, 17 July</td>
+            </tr>
+            <tr>
+              <td>AB123</td>
+              <td>Sony Charger</td>
+              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
+              <td>400</td>
+              <td>20</td>
+              <td>4</td>
+              <td>Mobile Accessories</td>
+              <td>16.45, 17 July</td>
+            </tr>
+            <tr>
+              <td>AB123</td>
+              <td>Sony Charger</td>
+              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
+              <td>400</td>
+              <td>20</td>
+              <td>4</td>
+              <td>Mobile Accessories</td>
+              <td>16.45, 17 July</td>
+            </tr>
+            <tr>
+              <td>AB123</td>
+              <td>Sony Charger</td>
+              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
+              <td>400</td>
+              <td>20</td>
+              <td>4</td>
+              <td>Mobile Accessories</td>
+              <td>16.45, 17 July</td>
+            </tr>
+            <tr>
+              <td>AB123</td>
+              <td>Sony Charger</td>
+              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
+              <td>400</td>
+              <td>20</td>
+              <td>4</td>
+              <td>Mobile Accessories</td>
+              <td>16.45, 17 July</td>
+            </tr>
+            <tr>
+              <td>AB123</td>
+              <td>Sony Charger</td>
+              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
+              <td>400</td>
+              <td>20</td>
+              <td>4</td>
+              <td>Mobile Accessories</td>
+              <td>16.45, 17 July</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 )
 
