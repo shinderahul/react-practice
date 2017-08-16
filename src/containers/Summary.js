1f7d9639cd71c28dfc0799c11d53ea130652ece9
LL\rahshind <rahul.shinde@digitaslbi.com>
2017-08-16 18:15:24 +0530

    Implemented Chart

--- src/containers/Summary.js
+++ src/containers/Summary.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Chart from '../components/Chart';
 
-const Summary = () => (
-  <div className="container">
+class Summary extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
 
-    <h2>This is Summary</h2>
+  componentWillMount(){
+    this.getChartData();
+  }
 
-  </div>
-)
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Clothing', 'Electronics', 'Accessories', 'Home'],
+        datasets:[
+          {
+            label:'Category',
+            data:[
+              150,
+              225,
+              212,
+              110
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="summary">
+        <h2>Summary</h2>
+        <Chart chartData={this.state.chartData} legendPosition="bottom"/>
+      </div>
+    );
+  }
+}
 
 export default Summary;
