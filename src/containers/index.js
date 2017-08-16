7f8a2503b6cbceadae20bfce9f38959cf903e6c9
LL\rahshind <rahul.shinde@digitaslbi.com>
2017-08-16 11:51:37 +0530

    Added Stateful component
    Added bootstrap dependency

---
+++ src/containers/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+   render() {
+      return (
+        <div>
+          <div className="dashboard container">
+            <div className="row">
+              <p>This is dashboard</p>
+            </div>
+          </div>
+        </div>
+      );
+   }
+}
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css'
+import Dashboard from './containers/';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
