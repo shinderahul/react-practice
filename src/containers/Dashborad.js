64ac517302e86f8fa1e8f3c4b8f651ec36df1299
LL\rahshind <rahul.shinde@digitaslbi.com>
2017-08-31 14:10:41 +0530

    Load json data in Dashboard component

--- src/containers/Dashborad.js
+++ src/containers/Dashborad.js
@@ -1,99 +1,71 @@
 import React from 'react';
+import axios from 'axios';
 
-const Dashboard = () => (
-  <div className="container">
-    <div className="row">
-      <div className="xs-12">
-        <h2>Dashboard</h2>
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Item Code</th>
-              <th>Item Name</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Max. Order</th>
-              <th>Category</th>
-              <th>Date Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>AB123</td>
-              <td>Sony Charger</td>
-              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
-              <td>400</td>
-              <td>20</td>
-              <td>4</td>
-              <td>Mobile Accessories</td>
-              <td>16.45, 17 July</td>
-            </tr>
-            <tr>
-              <td>AB123</td>
-              <td>Sony Charger</td>
-              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
-              <td>400</td>
-              <td>20</td>
-              <td>4</td>
-              <td>Mobile Accessories</td>
-              <td>16.45, 17 July</td>
-            </tr>
-            <tr>
-              <td>AB123</td>
-              <td>Sony Charger</td>
-              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
-              <td>400</td>
-              <td>20</td>
-              <td>4</td>
-              <td>Mobile Accessories</td>
-              <td>16.45, 17 July</td>
-            </tr>
-            <tr>
-              <td>AB123</td>
-              <td>Sony Charger</td>
-              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
-              <td>400</td>
-              <td>20</td>
-              <td>4</td>
-              <td>Mobile Accessories</td>
-              <td>16.45, 17 July</td>
-            </tr>
-            <tr>
-              <td>AB123</td>
-              <td>Sony Charger</td>
-              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
-              <td>400</td>
-              <td>20</td>
-              <td>4</td>
-              <td>Mobile Accessories</td>
-              <td>16.45, 17 July</td>
-            </tr>
-            <tr>
-              <td>AB123</td>
-              <td>Sony Charger</td>
-              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
-              <td>400</td>
-              <td>20</td>
-              <td>4</td>
-              <td>Mobile Accessories</td>
-              <td>16.45, 17 July</td>
-            </tr>
-            <tr>
-              <td>AB123</td>
-              <td>Sony Charger</td>
-              <td>Charger with 2 A, dash charge. Some additional info. Some tech spec.</td>
-              <td>400</td>
-              <td>20</td>
-              <td>4</td>
-              <td>Mobile Accessories</td>
-              <td>16.45, 17 July</td>
-            </tr>
-          </tbody>
-        </table>
+class Dashboard extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      items: []
+    };
+  }
+
+  componentWillMount(){
+    // Make a request for Data
+    axios
+      .get('https://raw.githubusercontent.com/shinderahul/react-practice/master/src/json/inventary.json')
+      //.get('https://www.reddit.com/r/reactjs.json')
+      .then(({ data })=> {
+      	this.setState({
+          items: data
+        });
+      })
+      .catch((err)=> {})
+  }
+  render() {
+
+    const dataItems = this.state.items.map((item) => {
+      return(
+        <tr key={item.id}>
+          <td>{item.itemCode}</td>
+          <td>{item.itemName}</td>
+          <td>{item.description}</td>
+          <td>{item.price}</td>
+          <td>{item.quantity}</td>
+          <td>{item.maxOrder}</td>
+          <td>{item.category}</td>
+          <td>{item.dateTime}</td>
+        </tr>
+      )
+    });
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="xs-12">
+            <h2>Dashboard</h2>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Item Code</th>
+                  <th>Item Name</th>
+                  <th>Description</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Max. Order</th>
+                  <th>Category</th>
+                  <th>Date Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataItems}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    );
+  }
+}
 
 export default Dashboard;
