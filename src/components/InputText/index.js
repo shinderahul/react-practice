8b775b4645db5162da13fe9a77fb348df5b3bf57
Rahul Shinde <rahshind@INMUBLBI0195.local>
2017-09-01 15:57:57 +0530

    Fixed bug in InputText

--- src/components/InputText/index.js
+++ src/components/InputText/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 class InputText extends React.Component {
    render() {
       return (
-        <input type="text" value="" placeholder={this.props.placeholder} className="form-control" />
+        <input type="text" value={this.props.value} placeholder={this.props.placeholder} className="form-control" />
       );
    }
 }
